dcdb54dca5779b3417ac80731c0c14520823df3d
Lloyd Sparkes <lloyd@lloydsparkes.co.uk>
2015-12-13 10:42:10 +0000

    Initial Commit of Projects

---
+++ CountdownNumberSolver/Program.fs
@@ -0,0 +1,63 @@
+﻿open System
+
+//Install From: http://symbolics.mathdotnet.com/
+open MathNet.Symbolics
+
+let random = Random()
+
+let validLargeNumbers = [|25; 50; 75; 100|]
+let validSmallNumbers = [|1; 1; 2; 2; 3; 3; 4; 4; 5; 5; 6; 6; 7; 7; 8; 8; 9; 9; 10; 10|]
+let validSize = [|3; 5; 7; 9; 11|]
+
+
+let randomExpression(number : seq<int>, operators: seq<string>) = seq { for n in 0..10 do if n % 2 = 0 then yield (Seq.nth (n/2) number).ToString() else yield Seq.nth (random.Next(0, 3)) operators}
+
+let swap (a: _[]) x y = 
+    let tmp = a.[x]
+    a.[x] <- a.[y]
+    a.[y] <- tmp
+
+let shuffle a = 
+    let temp = Seq.toArray a
+    Array.iteri (fun i _ -> swap temp i (random.Next(i, Array.length temp))) temp       
+    temp
+    
+type Solution(expression) = 
+    member this.expression = expression
+    member this.getExpressionString = Seq.fold (fun str x -> str + x) "" this.expression
+    member this.getParsed = Infix.parseOrThrow this.getExpressionString
+    member this.getIntegerResult = Int32.Parse(Infix.printStrict this.getParsed)
+    member this.getRating target = 
+        match this.getIntegerResult with
+           | x when x = target -> 10
+           | x when x > (target - 5) && x < (target + 5) -> 7
+           | x when x > (target - 10) && x < (target + 10) -> 5
+           | _ -> 0
+
+let innerGenerator(numbers: seq<int>) = seq { for n in 0..10 do if n % 2 = 0 then yield (Seq.nth (n/2) numbers).ToString() else yield Seq.nth (random.Next(0, 3)) [|"+"; "-"; "*"; "/"|]}
+let outerGenerator(numbers: seq<int>) = (Seq.take (validSize.[random.Next(0,4)]) (innerGenerator numbers))
+
+type RandomSolution(numbers: seq<int>) = inherit Solution(Seq.toArray (outerGenerator numbers))
+
+let randomGeneration(numbers: seq<int>) = seq { for n in 0..500000 do yield RandomSolution((shuffle numbers)) }
+
+[<EntryPoint>]
+let main argv = 
+    let largeToPick = random.Next(0, 4)
+    printfn "Picking %i large numbers" largeToPick
+    let smallToPick = 6 - largeToPick
+    let numbers = Seq.toArray (Seq.concat [|(Seq.take largeToPick (shuffle validLargeNumbers)); (Seq.take smallToPick (shuffle validSmallNumbers))|])
+    printfn "Large Numbers: %A" validLargeNumbers
+    printfn "Small Numbers: %A" validSmallNumbers
+    printfn "Numbers Picked: %A" (Array.sort numbers)
+    let target = random.Next(100, 999)
+    printfn "Target is: %i" target
+
+    for s in Seq.filter (fun (x: RandomSolution) -> (x.getRating target) > 0) (randomGeneration numbers) do
+        printfn "%s has Result %i Scores %i" s.getExpressionString s.getIntegerResult (s.getRating target)
+
+    printfn "Done"
+
+    let x = System.Console.ReadLine()
+    0
+            
---
+++ GchqSolver/Program.fs
@@ -0,0 +1,79 @@
+﻿open System
+open System.IO
+
+type CellType = Black | Blank | BlackFixed
+
+type Problem(size: int) = 
+    member this.size = size
+    member val grid = Array2D.create<CellType> size size CellType.Blank
+    member val rows = [] with get, set
+    member val columns = [] with get, set
+
+let loadFile(name: string) = 
+    let mutable p = Problem(0)
+    for row in File.ReadAllLines(name) do
+        let bits = row.Split ' '
+        if bits.[0] = "S" then p <- Problem(Int32.Parse(bits.[1]))
+        if bits.[0] = "Y" then p.rows <- p.rows @ [bits]
+        if bits.[0] = "X" then p.columns <- p.columns @ [bits]
+        if bits.[0] = "F" then p.grid.[Int32.Parse(bits.[1])-1,Int32.Parse(bits.[2])-1] <- CellType.BlackFixed
+    p
+
+let printGrid grid =
+    let maxY = (Array2D.length2 grid) - 1
+    let maxX = (Array2D.length1 grid) - 1
+    
+    for row in 0 .. maxY do
+        for col in 0 .. maxX do
+            if grid.[col, row] = CellType.BlackFixed then Console.Write("|F")
+            else if grid.[col, row] = CellType.Black then Console.Write("|B")
+            else Console.Write("| ")
+        Console.WriteLine("|")
+    Console.WriteLine()
+
+let shadeRowFromPosition(p:Problem, startPosition:int, numberToFill:int, rowIndex: int) = 
+    let mutable startPos = startPosition
+    while p.grid.[Math.Min(startPos+numberToFill+1, 24), rowIndex] = CellType.BlackFixed do
+        startPos <- startPos+1
+    for i in startPos..((startPos+numberToFill)-1) do
+        if p.grid.[i, rowIndex] <> CellType.BlackFixed then
+            p.grid.[i, rowIndex] <- CellType.Black
+    startPos + numberToFill + 1
+
+let shiftRowFromPosition(p:Problem, row:int, startPos:int) = 
+    for x in 24..(startPos+1) do
+        p.grid.[x, row] <- p.grid.[x-1, row]
+    p.grid.[startPos, row] <- CellType.Blank
+
+[<EntryPoint>]
+let main argv = 
+    let problem = loadFile("Puzzle.txt")
+    printGrid problem.grid
+
+    for rowIndex in 0 .. (problem.size-1) do
+        let mutable currIndex = 0
+        let items = problem.rows.Item rowIndex
+        for rowBlock in items do
+            if rowBlock <> "Y" then
+                currIndex <- shadeRowFromPosition(problem, currIndex, Int32.Parse rowBlock, rowIndex)
+
+    printGrid problem.grid
+
+    for colIndex in 0 .. (problem.size-1) do
+        let mutable currRow = 0
+        let items = problem.columns.Item colIndex 
+        for colBlock in items do
+            if colBlock <> "X" then
+                let blockSize = Int32.Parse colBlock
+                while currRow < 24 && problem.grid.[colIndex, currRow] = CellType.Blank do
+                    currRow <- currRow + 1
+                currRow <- currRow + blockSize
+                if currRow < 25 && problem.grid.[colIndex, currRow] <> CellType.Blank then
+                    shiftRowFromPosition(problem, currRow, colIndex)
+        for rowToShift in currRow..24 do
+            shiftRowFromPosition(problem, rowToShift, colIndex)
+
+    printGrid problem.grid
+
+    Console.ReadLine()
+    0 // return an integer exit code
